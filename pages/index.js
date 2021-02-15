ade10af30d45212547643beefc08ad80caad2d8a
Daniel-Bichof <Daniel-Bichof@gmail.com>
2021-02-15 18:23:46 -0300

    test getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,11 @@ function Home() {
         <Link href="/sobre">
             <a>Acessar página Sobre</a>
         </Link>
+        <br />
+        <Link href="/tempo">
+            <a>Acessar página Tempo</a>
+        </Link>
     </div>)
 }
 
-export default Home
+export default Home;
